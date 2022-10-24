594abdaffda2c4d476c5879e0e8007a1563d2b2b
Manuel Parra <manuel.parra1@gmail.com>
2022-10-24 12:13:04 -0500

    tables exercise answers

---
+++ tables_exercises.sql
@@ -0,0 +1,21 @@
+SHOW DATABASES;
+USE employees;
+SHOW TABLES;
+DESCRIBE employees;
+/*
+Data Types in employees table.
+
+int, date, varchar, varchar, enum, date
+
+
+Salaries table might have a numeric type column.
+
+Titles table  might contain a string type column.
+
+Employees table has a hire_date field so it would contain a date type column.
+
+*/
+
+-- employee and departments has no relation
+
+SHOW CREATE TABLE dept_manager;
